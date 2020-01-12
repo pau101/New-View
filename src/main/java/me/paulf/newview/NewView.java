0843da23b87d5cdc18e7f3f4500ac28a387f2e83
Paul Fulham <paul.fulham0@gmail.com>
2020-01-12 12:31:26 -0800

    Initial commit

---
+++ src/main/java/me/paulf/newview/NewView.java
@@ -0,0 +1,11 @@
+package me.paulf.newview;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NewView.ID)
+public final class NewView {
+	public static final String ID = "newview";
+
+	public NewView() {
+	}
+}
